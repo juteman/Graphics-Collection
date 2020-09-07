23896fe97560aeaf38b35e4c96fab9b551b6b067
juteman <hhjuteman@gmail.com>
2020-09-07 16:38:05 +0800

    Changes Rect shape

--- Rasterization/src/Rasterization.cpp
+++ Rasterization/src/Rasterization.cpp
@@ -1,6 +1,4 @@
-﻿// Rasterization.cpp: 定义应用程序的入口点。
-//
-#define SDL_MAIN_HANDLED
+﻿#define SDL_MAIN_HANDLED
 #include "Renderer.h"
 
 
@@ -8,10 +6,10 @@ class Triangle : public Renderer
 {
     void Draw() override
     {
-        Point2 x1 =  {{200, 200}, {static_cast<int8>(200), 0, 150, static_cast<int8>(255)}};
-        Point2 x2 =  {{1000, 200}, {static_cast<int8>(100), 50, 200, static_cast<int8>(255)}};
-        Point2 x3 =  {{200, 1000}, {static_cast<int8>(10), 200, 70, static_cast<int8>(255)}};
-        Point2 x4 = {{1000, 1000}, {static_cast<int8>(250), 250, 250, static_cast<int8>(255)}};
+        Point2 x1 =  {{1000, 200}, {static_cast<int8>(200), 0, 150, static_cast<int8>(255)}};
+        Point2 x2 =  {{600, 500}, {static_cast<int8>(100), 50, 200, static_cast<int8>(255)}};
+        Point2 x3 =  {{1400, 600}, {static_cast<int8>(10), 200, 70, static_cast<int8>(255)}};
+        Point2 x4 = {{1000, 800}, {static_cast<int8>(250), 250, 250, static_cast<int8>(255)}};
         Renderer::Triangles(x1, x2, x3);
         Renderer::Triangles(x2, x3, x4);
     }
